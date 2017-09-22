050f094eda95a1d1bc84d29c001ffe328294a22b
Dulled <viniciusdupuit@gmail.com>
2017-09-22 16:57:02 -0300

    Atualização 01

--- lista01/01.c
+++ lista01/01.c
@@ -2,14 +2,13 @@
 
 int main(){
 	int x;
-	scanf("Entre com um valor inteiro: %d", &x);
-	if ( (x%2) == 0) {
+	scanf("%d", &x);
+	
+	if ( x%2 == 0) {
 		printf("Par\n");
 	}	
 	else{
 		printf("Impar\n");
 	}
-
-	return 0;
 }
 
