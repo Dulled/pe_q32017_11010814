dc7ca503a1d031bf62e20e56dd33169a2f89548a
ufabc <viniciusdupuit@gmail.com>
2017-10-06 09:57:18 -0300

    ble

---
+++ lista02/2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+float volume(float a, float b, float c)
+{
+	return a*b*c;
+}
+
+int main()
+{
+	float a, b, c;
+	scanf("%f %f %f", &a, &b, &c);
+	printf("%f\n", volume(a, b, c));
+	return 0;
+}
+
